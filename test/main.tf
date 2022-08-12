e7a2a164ecb7beb796a2f335ca79bc302fb6c59a
German <valdezgerman@icloud.com>
2022-08-11 22:42:24 -0700

    Version 0.1.2

--- test/main.tf
+++
@@ -1,8 +0,0 @@
-#API calls to get team ID based on name
-data "http" "get_team" {
-  for_each = var.teams
-  url      = "https://api.us1.signalfx.com/v2/team?name=${each.key}"
-  request_headers = {
-    X-SF-TOKEN = "${var.signalfx_auth_token}"
-  }
-}
